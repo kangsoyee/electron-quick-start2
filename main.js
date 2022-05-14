7b57f705fee2f8e4830051c66749a9376358af7c
kangse2942 <kangse2942@ohjic.com>
2022-05-14 17:43:24 +0900

    auto-updater

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const autoUpdater = require('electron-updater').autoUpdater
-autoUpdater.setFeedURL('git@bitbucket.org:ohjicin/ohjic-desktop-client.git')
+const ProgressBar = require('electron-progressbar');
+autoUpdater.setFeedURL('https://kangsoyee@bitbucket.org/ohjicin/ohjic-desktop-client.git')
 
 const path = require('path')
 const url = require('url')
@@ -57,30 +58,86 @@ app.on('window-all-closed', function () {
 
 
 
-autoUpdater.on('update-available', function () {
-  console.log('A new update is available')
-  contents.send('updater-message', 'A new update is available')
-})
-autoUpdater.on('checking-for-update', function () {
-  console.log('Checking-for-update')
-  contents.send('updater-message', 'Checking for Update..')
-})
-autoUpdater.on('error', function (error) {
-  console.log('error')
-  console.error(error)
-  contents.send('updater-message', 'Got Error')
-})
-autoUpdater.on('download-progress', function (bytesPerSecond, percent, total, transferred) {
-  console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
-  contents.send('updater-message', `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
-})
-autoUpdater.on('update-downloaded', function (event) {
-  console.log('update-downloaded')
-  console.log(event)
-  contents.send('updater-message', 'update-downloaded')
-})
+module.exports = () => {
+  autoUpdater.checkForUpdates();
 
-autoUpdater.on('update-not-available', function () {
-  console.log('update-not-available')
-  contents.send('updater-message', 'update-not-available')
-})
+  let progressBar;
+    autoUpdater.on('update-available', () => {
+        dialog
+            .showMessageBox({
+                type: 'info',
+                title: 'Update available',
+                message:
+                    'A new version of Project is available. Do you want to update now?',
+                buttons: ['Update', 'Later'],
+            })
+            .then((result) => {
+                const buttonIndex = result.response;
+
+                if (buttonIndex === 0) autoUpdater.downloadUpdate();
+            });
+    });
+/* progress bar가 없으면 업데이트를 다운받는 동안 사용자가 그 내용을 알 수 없기 때문에
+progress bar는 꼭 만들어준다. */
+autoUpdater.once('download-progress', (progressObj) => {
+  progressBar = new ProgressBar({
+    text: 'Downloading...',
+    detail: 'Downloading...',
+  });
+
+  progressBar
+      .on('completed', function () {
+        console.info(`completed...`);
+        progressBar.detail = 'Task completed. Exiting...';
+      })
+      .on('aborted', function () {
+        console.info(`aborted...`);
+      });
+});
+
+// 업데이트를 다운받고 나면 업데이트 설치 후 재시작을 요청하는 팝업이 뜬다.
+autoUpdater.on('update-downloaded', () => {
+  progressBar.setCompleted();
+  dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Update ready',
+        message: 'Install & restart now?',
+        buttons: ['Restart', 'Later'],
+      })
+      .then((result) => {
+        const buttonIndex = result.response;
+
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
+      });
+});
+};
+
+//
+// autoUpdater.on('update-available', function () {
+//   console.log('A new update is available')
+//   contents.send('updater-message', 'A new update is available')
+// })
+// autoUpdater.on('checking-for-update', function () {
+//   console.log('Checking-for-update')
+//   contents.send('updater-message', 'Checking for Update..')
+// })
+// autoUpdater.on('error', function (error) {
+//   console.log('error')
+//   console.error(error)
+//   contents.send('updater-message', 'Got Error')
+// })
+// autoUpdater.on('download-progress', function (bytesPerSecond, percent, total, transferred) {
+//   console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
+//   contents.send('updater-message', `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
+// })
+// autoUpdater.on('update-downloaded', function (event) {
+//   console.log('update-downloaded')
+//   console.log(event)
+//   contents.send('updater-message', 'update-downloaded')
+// })
+//
+// autoUpdater.on('update-not-available', function () {
+//   console.log('update-not-available')
+//   contents.send('updater-message', 'update-not-available')
+// })
